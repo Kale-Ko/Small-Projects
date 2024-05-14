c8bc6067fbb8e1bb4948bdc95346b73c8c46a2cb
Kale <github@kaleko.dev>
2024-05-14 16:13:41 -0400

    Update

--- Raytracer/src/test/java/io/github/kale_ko/fun/renderer/SoftwareGLTest.java
+++
@@ -1,8 +0,0 @@
-package io.github.kale_ko.fun.renderer;
-
-public class SoftwareGLTest extends SoftwareCommonTest {
-    public static void main(String[] args) {
-        SoftwareGL renderer = new SoftwareGL(SoftwareCommonTest.levelWidth, SoftwareCommonTest.levelHeight, SoftwareCommonTest.level, SoftwareCommonTest.fpsCap, SoftwareCommonTest.displayScale, SoftwareCommonTest.playWidth, SoftwareCommonTest.playHeight, SoftwareCommonTest.playScale, SoftwareCommonTest.playFov);
-        renderer.run();
-    }
-}
